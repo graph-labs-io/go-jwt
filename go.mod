690c06202746ae1b79a4562beb67df02d471a42b
FlowGoCrazy <thatflowgocrazy@gmail.com>
2021-08-08 19:31:53 -0400

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/graph-labs-io/go-jwt
+
+go 1.16
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/google/uuid v1.3.0
+)
---
+++ jwt.go
@@ -0,0 +1,105 @@
+package jwt
+
+import (
+	"errors"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/google/uuid"
+)
+
+type tokenClaims struct {
+	Key        string
+	Ip         string
+	TokenId    string
+	InstanceId string
+	jwt.StandardClaims
+}
+
+func GenerateToken(key string, ip string, instanceId string) (string, string, error) {
+	tokenId := uuid.New().String()
+
+	claims := tokenClaims{
+		key,
+		ip,
+		tokenId,
+		instanceId,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Minute * 15).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+	}
+
+	jsonWebToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedString, jwtSignErr := jsonWebToken.SignedString([]byte(os.Getenv("JWT_KEY")))
+	if jwtSignErr != nil {
+		return "", "", jwtSignErr
+	}
+
+	return signedString, tokenId, nil
+}
+
+func VerifyTokenSignature(tokenString string) (tokenClaims, error) {
+	parsedToken, jwtParseErr := jwt.ParseWithClaims(tokenString, &tokenClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("JWT_KEY")), nil
+	})
+
+	if jwtParseErr != nil {
+		return tokenClaims{}, jwtParseErr
+	}
+
+	if claims, ok := parsedToken.Claims.(*tokenClaims); ok && parsedToken.Valid {
+		return *claims, nil
+	} else {
+		return tokenClaims{}, errors.New("invalid token")
+	}
+}
+
+type TokenClaimsMismatch []TokenClaimComparison
+type TokenClaimComparison struct {
+	Claim    string
+	Received string
+	Expected string
+}
+
+type UserJwtRecord struct {
+	Ip         string `json:"ip"`
+	Key        string `json:"key"`
+	TokenId    string `json:"tokenId"`
+	InstanceId string `json:"instanceId"`
+}
+
+func VerifyTokenClaims(recievedTokenClaims tokenClaims, currentJwtRecord UserJwtRecord, requestIp string, requestInstanceId string) (bool, TokenClaimsMismatch) {
+	if recievedTokenClaims.TokenId == currentJwtRecord.TokenId && currentJwtRecord.InstanceId == requestInstanceId && currentJwtRecord.Ip == requestIp {
+		return true, TokenClaimsMismatch{}
+	} else {
+		mismatch := TokenClaimsMismatch{}
+
+		if recievedTokenClaims.TokenId != currentJwtRecord.TokenId {
+			mismatch = append(mismatch, TokenClaimComparison{
+				"TokenId",
+				recievedTokenClaims.TokenId,
+				currentJwtRecord.TokenId,
+			})
+		}
+
+		if requestInstanceId != currentJwtRecord.InstanceId {
+			mismatch = append(mismatch, TokenClaimComparison{
+				"InstanceId",
+				requestInstanceId,
+				currentJwtRecord.InstanceId,
+			})
+		}
+
+		if requestIp != currentJwtRecord.Ip {
+			mismatch = append(mismatch, TokenClaimComparison{
+				"Ip",
+				requestIp,
+				currentJwtRecord.Ip,
+			})
+		}
+
+		return false, mismatch
+	}
+}
